240f83fbf08e114158addaa5e9abc777a899ee3d
John Doe <webdevnorth@gmail.com>
2016-12-03 20:28:39 +0000

    updated css loader chain, removed missing components from router

--- src/client/routes/routes.js
+++ src/client/routes/routes.js
@@ -6,15 +6,11 @@ var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
 var MainContainer = require('../containers/MainContainer.js');
 var HomeContainer = require('../containers/HomeContainer.js');
-// var Login = require('../containers/LoginContainer.js');
-// var Register = require('../containers/RegisterContainer.js');
 
 var routes = (
 	<Router history={hashHistory}>
 		<Route path='/' component={MainContainer}>
 			<IndexRoute component={HomeContainer} />
-			<Route path='login' component={Login} />	
-			<Route path='register' component={Register} />	
 		</Route>
 	</Router>
 );
