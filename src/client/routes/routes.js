96ac6238f6522248387a3737066a2543ce22477a
John Doe <webdevnorth@gmail.com>
2017-01-23 18:11:52 +0000

    add 2 local fonts, add normalize scss partial, add fundamental styling, change routing to browserHistory

--- src/client/routes/routes.js
+++ src/client/routes/routes.js
@@ -3,12 +3,12 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
-var hashHistory = ReactRouter.hashHistory;
+var browserHistory = ReactRouter.browserHistory;
 var MainContainer = require('../containers/MainContainer.js');
 var HomeContainer = require('../containers/HomeContainer.js');
 
 var routes = (
-	<Router history={hashHistory}>
+	<Router history={browserHistory}>
 		<Route path='/' component={MainContainer}>
 			<IndexRoute component={HomeContainer} />
 		</Route>
