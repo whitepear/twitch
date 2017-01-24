d81754fa2a65e2979cddc10c19e22618ae0cf0dc
John Doe <webdevnorth@gmail.com>
2017-01-24 16:21:46 +0000

    Style Home component for sm-screens

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -4,17 +4,22 @@ var Home = React.createClass({
 	render: function() {
 		return (
 			<div className="container">
+				<div className="hidden-xs seats">
+					{ /*This div is used to display an image on sm+ screens*/ }
+				</div>
 				<div className="home-header-container">
 					<div className="home-header twitch-header-one"><span className="twitch-header-one-capital">T</span>witchTV</div>
 					<div className="home-header twitch-header-two"><span className="twitch-header-two-capital">V</span>iewer</div>
 					<h2 className="twitch-icon">&#xf1e8;</h2>
 				</div>
 				<div className="home-login-container">
+					<div className="hidden-xs home-login-header">Login</div>
 					<form action="/login" method="POST">
 						<input type="text" name="loginUsername" placeholder="Username" className="" />
 						<input type="password" name="loginPassword" placeholder="Password" className="" />
 						<button type="submit" id="homeLoginSubmit" className="btn">Login</button>
-					</form>					
+					</form>
+					<div id="loginMessage"></div>					
 				</div>
 				<div className="home-register-container">
 					<div className="home-register-message">Don't have an account?</div>
