20f1f0b57aa2edede5ad8089830b5bce7301e880
John Doe <webdevnorth@gmail.com>
2017-01-24 01:31:55 +0000

    create styled home component for xs-devices

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -3,7 +3,26 @@ var React = require('react');
 var Home = React.createClass({
 	render: function() {
 		return (
-			<div>Hello from Home.</div>
+			<div className="container">
+				<div className="home-header-container">
+					<div className="home-header twitch-header-one"><span className="twitch-header-one-capital">T</span>witchTV</div>
+					<div className="home-header twitch-header-two"><span className="twitch-header-two-capital">V</span>iewer</div>
+					<h2 className="twitch-icon">&#xf1e8;</h2>
+				</div>
+				<div className="home-login-container">
+					<form action="/login" method="POST">
+						<input type="text" name="loginUsername" placeholder="Username" className="" />
+						<input type="password" name="loginPassword" placeholder="Password" className="" />
+						<button type="submit" id="homeLoginSubmit" className="btn">Login</button>
+					</form>					
+				</div>
+				<div className="home-register-container">
+					<div className="home-register-message">Don't have an account?</div>
+					<a href="/register">
+						<button type="button" className="btn home-register-btn">Register</button>
+					</a>
+				</div>
+			</div>
 		)
 	}
 });
