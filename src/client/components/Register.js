0a555b88f5d6a0448f40c111e26530ef8b1f9dc2
John Doe <webdevnorth@gmail.com>
2017-01-28 19:13:43 +0000

    add registration-related functionality to register component

--- src/client/components/Register.js
+++ src/client/components/Register.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var Link = require('react-router').Link;
+var PropTypes = React.PropTypes;
 
 function Register(props) {
 	return (
@@ -8,7 +9,7 @@ function Register(props) {
 				<div className="register-banner-text">Register</div>
 			</div>
 			<div className="register-form-container">
-				<form action="/register" method="POST" className="register-form">
+				<form action="/register" method="POST" onSubmit={props.onRegisterSubmit} className="register-form">
 					<label htmlFor="registerEmail">Email:</label>
 					<input type="text" id="registerEmail" required />
 					<label htmlFor="registerUsername">Username:</label>
@@ -17,13 +18,22 @@ function Register(props) {
 					<input type="password" id="registerPassword" placeholder="8 characters minimum." required />
 					<label htmlFor="registerPasswordRepeat">Repeat Password:</label>
 					<input type="password" id="registerPasswordRepeat" placeholder="Letters & numbers only." required />
-					<div id="registerFormMessage"></div>
-					<button type="submit" className="btn">Register</button>
+					<div id="registerFormMessage">
+						{props.validationMessage}
+					</div>
+					<button type="submit" className="btn" disabled={props.validationPassed}>Register</button>
 					<Link to="/" className="register-cancel">Cancel</Link>
 				</form>
 			</div>
 		</div>
 	)
+}
+
+Register.propTypes = {
+	onFieldChange: PropTypes.func.isRequired,
+	onRegisterSubmit: PropTypes.func.isRequired,
+	validationMessage: PropTypes.string.isRequired,
+	validationPassed: PropTypes.bool.isRequired
 }
 
 module.exports = Register;
--- src/client/containers/RegisterContainer.js
+++ src/client/containers/RegisterContainer.js
@@ -1,9 +1,83 @@
 var React = require('react');
 var Register = require('../components/Register.js');
+var registrationValidation = require('../utils/clientValidation.js').registrationValidation;
+var axios = require('axios');
 
 var RegisterContainer = React.createClass({
+	contextTypes: {
+		router: React.PropTypes.object.isRequired
+	},
+	getInitialState: function() {
+		return {			
+			registerEmail: '',
+			registerUsername: '',
+			registerPassword: '',
+			registerPasswordRepeat: '',
+			validationMessage: '',
+			validationPassed: false
+		};
+	},
+	handleFieldChange: function(e) {
+		this.setState({
+			[e.target.id]: e.target.value
+		});
+	},
+	handleRegisterSubmit: function(e) {
+		e.preventDefault();
+		var validationResult = registrationValidation(this.state);
+
+		if (validationResult.validationPassed) {
+			// passed client-side validation, call handleServerSubmission to send
+			// to server for further validation and registration
+
+			// disable registration submit button while server-side 
+			// validation takes place
+			this.setState({
+				validationPassed: true
+			}, this.handleServerSubmission);
+		} else {
+			// failed client-side validation
+			this.setState({
+				validationMessage: validationResult.validationMessage
+			});
+		}
+	},
+	handleServerSubmission: function() {
+		// pass form-data to server for validation and potential registration
+		axios.post('/register', this.state)
+		.then(function(serverRes) {
+			if (res.data.serverValidationPassed) {
+				// validation passed, user registered
+				// redirect user				
+				this.setState({
+					validationMessage: res.data.serverValidationMessage
+				}, function() {
+					setTimeout(function() {
+						this.context.router.push('/');						
+					}.bind(this), 1500);
+				});
+			} else {
+				// failed server validation
+				this.setState({
+					validationMessage: res.data.serverValidationMessage,
+					validationPassed: false					
+				});
+			}
+		}.bind(this))
+		.catch(function(err) {
+			console.log(err);
+			this.setState({
+				validationMessage: 'A server error occurred while processing your request. Please try again later.',
+				validationPassed: false
+			});
+		}.bind(this));
+	},
 	render: function() {
-		return <Register />
+		return <Register 
+							onFieldChange={this.handleFieldChange} 
+							onRegisterSubmit={this.handleRegisterSubmit}
+							validationMessage={this.state.validationMessage}
+							validationPassed={this.state.validationPassed} />
 	}
 });
 
