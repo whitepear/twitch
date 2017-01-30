7acca3fd489889ae2f4957c90a83a3058fe5646b
John Doe <webdevnorth@gmail.com>
2017-01-30 00:40:20 +0000

    create middleware for sanitizing body, queries and params on req object

--- src/server/utils/routeMiddleware.js
+++ src/server/utils/routeMiddleware.js
@@ -1,3 +1,43 @@
+// this file contains middleware functions that santiize client input,
+// or check the client's login status
+
+var sanitizeHtml = require('sanitize-html');
+
+function sanitizeUserInput(req, res, next) {
+	// escape user input
+	var bodyKeys = Object.keys(req.body);
+	var paramKeys = Object.keys(req.params);
+	var queryKeys = Object.keys(req.query);
+	var escapedContent; // loop variable for storing escaped strings
+
+	bodyKeys.forEach(function(key) {
+		escapedContent = sanitizeHtml(req.body[key], {
+			allowedTags: [],
+			allowedAttributes: []
+		});
+		req.body[key] = escapedContent.replace(/&quot;/g, '"');
+	});
+
+	paramKeys.forEach(function(key) {
+		escapedContent = sanitizeHtml(req.params[key], {
+			allowedTags: [],
+			allowedAttributes: []
+		});
+		req.params[key] = escapedContent.replace(/&quot;/g, '"');
+	});
+
+	queryKeys.forEach(function(key) {
+		escapedContent = sanitizeHtml(req.query[key], {
+			allowedTags: [],
+			allowedAttributes: []
+		});
+		req.query[key] = escapedContent.replace(/&quot;/g, '"');
+	});
+
+	next();
+}
+
+
 function loggedOut(req, res, next) {
 	// prevent logged in users from accessing a route
   if (req.session && req.session.userId) {
@@ -8,6 +48,7 @@ function loggedOut(req, res, next) {
   	next();
   }
 }
+
 
 function loggedIn(req, res, next) {
 	// prevent unauthenticated users from accessing a route
