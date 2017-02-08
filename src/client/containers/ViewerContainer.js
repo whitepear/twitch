e709bc9c2304efead625d70927d49a00c3a1cb9b
John Doe <webdevnorth@gmail.com>
2017-02-08 22:27:03 +0000

    implement viewer-container

--- src/client/containers/ViewerContainer.js
+++ src/client/containers/ViewerContainer.js
@@ -1,8 +1,42 @@
 var React = require('react');
+var SearchContainer = require('./SearchContainer.js');
+var FavouritesContainer = require('./FavouritesContainer.js');
+var axios = require('axios');
 
 var ViewerContainer = React.createClass({
+	contextTypes: {
+		router: React.PropTypes.object.isRequired
+	},
+	getInitialState: function() {
+		return {
+			activeTab: 'Search'
+		};
+	},
+	handleLogOut: function(e) {
+		axios.post('/logOut')
+		.then(function() {
+			this.context.router.push('/');
+		}.bind(this))
+		.catch(function(err) {
+			console.log('/logOut POST error: ', err);
+		});
+	},
 	render: function() {
-		return <div>Viewer</div>
+		return (
+			<div className="container">
+				<div className="viewer-nav">
+					<div className="twitch-logo">&#xf1e8;</div>
+					<div className="log-out-container">
+						<button className="log-out-btn" onClick={this.handleLogOut}>Log Out</button>
+					</div>						
+				</div>
+				<div className="tab-container">
+					<div className={ (this.state.activeTab === 'Search' ? 'active-tab ' : '' ) + "tab" }>Search</div>
+					<div className={ (this.state.activeTab === 'Favourites' ? 'active-tab ' : '' ) + "tab" }>Favourites</div>
+				</div>
+				{this.state.activeTab === 'Search' ? <SearchContainer /> : <FavouritesContainer />}
+			</div>
+		);
 	}
 });
 
