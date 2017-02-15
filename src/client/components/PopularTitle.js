419f1cb5b3912b40b04e7ba8a054091759756272
John Doe <webdevnorth@gmail.com>
2017-02-15 17:41:37 +0000

    style PopularTitle for sm-devices

--- src/client/components/PopularTitle.js
+++ src/client/components/PopularTitle.js
@@ -5,9 +5,20 @@ var PropTypes = React.PropTypes;
 // used by getTopGames method within SearchContainer.
 
 function PopularTitle(props) {
+	// get appropriately-sized image based on screen-width
+	if (window.innerWidth < 768) {
+		var gameBoxImg = props.gameInfo.game.box.medium;
+	} else if (window.innerWidth < 992) {
+		gameBoxImg = props.gameInfo.game.box.large;
+	} else if (window.innerWidth < 1200) {
+		gameBoxImg = 'https://static-cdn.jtvnw.net/ttv-boxart/Counter-Strike:%20Global%20Offensive-{width}x{height}.jpg';
+	} else {
+		gameBoxImg = 'https://static-cdn.jtvnw.net/ttv-boxart/Counter-Strike:%20Global%20Offensive-{width}x{height}.jpg';
+	}
+	
 	return (
 		<div className="popular-title-container">
-			<img src={props.gameInfo.game.box.medium} className="popular-title-image" alt="Title box-art"/>
+			<img src={gameBoxImg} className="popular-title-image" alt="Title box-art"/>
 			<div className="popular-title-description">
 				<div className="popular-title-name">{props.gameInfo.game.name}</div>
 				<div className="popular-title-viewers"><span className="eye-symbol">&#xe900;</span> {props.gameInfo.viewers}</div>
