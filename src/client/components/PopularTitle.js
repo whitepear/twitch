ad85a604d12ba0c5eef8d54117ee8f1b0f76575f
John Doe <webdevnorth@gmail.com>
2017-02-16 15:49:58 +0000

    style ViewerContainer and SearchContainer for md-devices

--- src/client/components/PopularTitle.js
+++ src/client/components/PopularTitle.js
@@ -8,13 +8,9 @@ function PopularTitle(props) {
 	// get appropriately-sized image based on screen-width
 	if (window.innerWidth < 768) {
 		var gameBoxImg = props.gameInfo.game.box.medium;
-	} else if (window.innerWidth < 992) {
-		gameBoxImg = props.gameInfo.game.box.large;
-	} else if (window.innerWidth < 1200) {
-		gameBoxImg = 'https://static-cdn.jtvnw.net/ttv-boxart/Counter-Strike:%20Global%20Offensive-{width}x{height}.jpg';
 	} else {
-		gameBoxImg = 'https://static-cdn.jtvnw.net/ttv-boxart/Counter-Strike:%20Global%20Offensive-{width}x{height}.jpg';
-	}
+		gameBoxImg = props.gameInfo.game.box.large;
+	} 
 	
 	return (
 		<div className="popular-title-container">
